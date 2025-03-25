5024862e8326cd186d74b4972efd8b847fc57419
doo <48028279+fandugem@users.noreply.github.com>
2025-03-25 09:05:42 +0700

    Update script.js
    Signed-off-by: doo <48028279+fandugem@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.getElementById("toggle-darkmode")
 .addEventListener("click", function() {
-    console.log("Tombol ditekan");
-    document.body.classList.toggle("dark-mode");
-});console.log("Dark mode button clicked!");document.getElementById("toggle-darkmode").addEventListener("click", function() {
+    console.log("Dark mode button clicked!");document.getElementById("toggle-darkmode").addEventListener("click", function() {
     alert("Button clicked!"); 
     document.body.classList.toggle("dark-mode");
 });
