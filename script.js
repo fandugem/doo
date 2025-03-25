b618d9644a607ec4adf464b4ef656996073093c7
doo <48028279+fandugem@users.noreply.github.com>
2025-03-26 01:23:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("toggle-darkmode");
-    button.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
-    });
+const toggleButton = document.getElementById("toggle-darkmode");
+const darkModeIcon = document.getElementById("darkmode-icon");
+
+const lightIcon = "img/light-mode-icon.png"; // Ganti dengan nama file mode terang
+const darkIcon = "img/dark-mode-icon.png"; // Ganti dengan nama file mode gelap
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    // Ganti icon
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeIcon.src = darkIcon;
+    } else {
+        darkModeIcon.src = lightIcon;
+    }
 });
-
-
