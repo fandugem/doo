38035e8d608deab47536dda502fe32a83f09f237
doo <48028279+fandugem@users.noreply.github.com>
2025-03-26 02:16:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const toggleButton = document.getElementById("toggle-darkmode");
 const darkModeIcon = document.getElementById("darkmode-icon");
 
-const lightIcon = "img/dark.png"; // Ganti dengan nama file mode terang
-const darkIcon = "img/white.png"; // Ganti dengan nama file mode gelap
+const lightIcon = "img/dark.png"; // Ganti nama filenya
+const darkIcon = "img/white.png"; // Ganti nama filenya
+
+// Cek dark mode di localStorage (biar tetap kepake setelah refresh)
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    darkModeIcon.src = darkIcon;
+}
 
 toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     
-    // Ganti icon
     if (document.body.classList.contains("dark-mode")) {
         darkModeIcon.src = darkIcon;
+        localStorage.setItem("darkMode", "enabled");
     } else {
         darkModeIcon.src = lightIcon;
+        localStorage.setItem("darkMode", "disabled");
     }
 });
