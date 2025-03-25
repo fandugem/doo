a4d4c2e552bc694f9b462a26903423e6b4ddffa3
fandugem <48028279+fandugem@users.noreply.github.com>
2025-03-25 08:51:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("toggle-darkmode")
+.addEventListener("click", function() {
+    console.log("Tombol ditekan");
+    document.body.classList.toggle("dark-mode");
+});console.log("Dark mode button clicked!");document.getElementById("toggle-darkmode").addEventListener("click", function() {
+    alert("Button clicked!"); 
+    document.body.classList.toggle("dark-mode");
+});
