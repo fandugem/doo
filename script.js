8fdd0af9dfeb0092b0deb254d0e0d5751afbec54
fandugem <48028279+fandugem@users.noreply.github.com>
2025-03-25 08:12:09 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("toggle-darkmode");
-    button.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
-    });
-});
