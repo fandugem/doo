f9532aa1133548b547fa7f06c325dd1e8bfdfcb4
doo <48028279+fandugem@users.noreply.github.com>
2025-03-26 16:49:42 +0700

    Update script.js

--- hype/script.js
+++ hype/script.js
@@ -2,8 +2,7 @@ window.onload = function() {
     const video = document.getElementById('background-video');
 
     // Example of changing video source
-    video.src = "/img
-/mylivewallpapers-com-Green-Dragon-Roronoa-Zoro-4K.mp4";
+    video.src = "img/mylivewallpapers-com-Green-Dragon-Roronoa-Zoro-4K.mp4";
     video.load();  // Reload the video to apply the change
 };
 
