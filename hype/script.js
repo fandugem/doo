458c330fde5fc53173bfaeab679da15216e0eab2
doo <48028279+fandugem@users.noreply.github.com>
2025-03-26 16:32:09 +0700

    Update script.js

--- hype/script.js
+++ hype/script.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    const video = document.getElementById('background-video');
+
+    // Example of changing video source
+    video.src = "path-to-another-video.mp4";
+    video.load();  // Reload the video to apply the change
+};
+
 document.body.addEventListener("click", function() {
     alert("You clicked the page!");
 });
