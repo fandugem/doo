6bbb682a111a38befdaf250c92238d6d74ffed56
doo <48028279+fandugem@users.noreply.github.com>
2025-03-29 12:47:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateDarkModeIcon() {
         if (darkModeToggle) {
-            darkModeToggle.src = body.classList.contains("dark-mode") ? "img/dark.png" : "img/white.png";
+            darkModeToggle.src = body.classList.contains("dark-mode") ? "img/bodas.png" : "img/hideng.png";
         }
     }
 
