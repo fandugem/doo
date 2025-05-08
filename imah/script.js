d75832e656c1c8a0e7edd5e837491bb9e979d32b
doo <48028279+fandugem@users.noreply.github.com>
2025-05-08 16:02:27 +0700

    Update script.js

--- imah/script.js
+++ imah/script.js
@@ -64,7 +64,7 @@ function goToSlide(index) {
     fetch(`chapter/chapter${index}.html`)
       .then(res => res.text())
       .then(html => {
-        chapterContainer.innerHTML = '<div class="slide active">' + html + '</div>';
+        chapterContainer.innerHTML = '<div class="slide active"><div class="story-section">' + html + '</div></div>';
         const buttons = document.querySelectorAll('.page-buttons button');
         buttons[buttons.length - 1].classList.add('active');
 
